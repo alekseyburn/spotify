e136150d145931bc327e6dde7516fc0b53c2a1ac
Aleksey Burn <41241809+alekseyburn@users.noreply.github.com>
2023-07-31 23:56:57 +0700

    init#2

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router', '@vueuse/core'],
+      dts: true,
+      dirs: ['./src/composables'],
+      vueTemplate: true,
+    }),
+    Components({
+      dts: true,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
